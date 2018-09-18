8a0b97b4f79e6501f6b0681680d4bcbf0c82d027
91juhwang <91juhwang@gmail.com>
2018-09-17 21:57:59 -0400

    Add verification route for Slack verification

--- app.rb
+++ app.rb
@@ -1,5 +1,12 @@
 require 'sinatra'
+require 'awesome_print'
 
 get '/' do
   "Hello World!"
+end
+
+get '/events' do
+  challenge_code = params[:challenge_code]
+  status 200
+  body challenge_code
 end
